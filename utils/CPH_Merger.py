2af80a2856ff5365a1ce283cfd256201527b00af
elespike <elespike@lab26.net>
2019-10-05 13:24:17 -0400

    CPH_Merger.py is now properly cross-platform General enhancements to code quality

--- utils/CPH_Merger.py
+++ utils/CPH_Merger.py
@@ -1,49 +1,52 @@
-from os  import path, chdir, SEEK_CUR
-from sys import argv
+from os  import path, chdir
+from sys import exit
 
 
-chdir(path.dirname(path.abspath(argv[0])))
-cph_help   = open('../CPH_Help.py'          , 'r')
-cph_config = open('../CPH_Config.py'        , 'r')
-cph_main   = open('../CustomParamHandler.py', 'r')
-tinyweb    = open('../tinyweb.py'           , 'r')
+if __name__ != '__main__':
+    exit('[-] CPH_Merger.py should be invoked directly, not imported!')
 
+def is_comment_or_whitespace(line):
+    line = line.strip()
+    if not line or line.startswith('#'):
+        return True
+    return False
 
-def write_imports(opened_file):
-    line = opened_file.readline()
+def write_imports(source_file, destination_file):
+    line = source_file.readline()
     while line:
         if line.startswith('class'):
-            opened_file.seek(len(line) * -1, SEEK_CUR)
-            merged_file.write('\n')
+            source_file.seek(source_file.tell() - len(line))
             break
-        if line.strip().startswith('from CPH_Config')\
-        or line.strip().startswith('from CPH_Help'  )\
+        if line.strip().startswith('from CPH_Help'  )\
+        or line.strip().startswith('from CPH_Config')\
         or line.strip().startswith('from tinyweb'   )\
-        or line.strip().startswith('#'              )\
-        or not line.strip():
-            line = opened_file.readline()
+        or is_comment_or_whitespace(line):
+            line = source_file.readline()
             continue
-        merged_file.write(line)
-        line = opened_file.readline()
+        destination_file.write(line)
+        line = source_file.readline()
 
-with open('../CustomParamHandler_merged.py', 'w') as merged_file:
-    write_imports(cph_help)
-    write_imports(tinyweb)
-    write_imports(cph_config)
-    write_imports(cph_main)
+chdir(path.dirname(path.abspath(__file__)))
 
-    for line in cph_help.readlines():
-        merged_file.write(line)
-    for line in tinyweb.readlines():
-        merged_file.write(line)
-    for line in cph_config.readlines():
-        merged_file.write(line)
-    for line in cph_main.readlines():
-        merged_file.write(line)
+try:
+    source_files = [
+        open('../CPH_Config.py'        , 'rb'),
+        open('../CPH_Help.py'          , 'rb'),
+        open('../CustomParamHandler.py', 'rb'),
+        open('../tinyweb.py'           , 'rb'),
+    ]
 
+    with open('../CustomParamHandler_merged.py', 'wb') as destination_file:
+        for source_file in source_files:
+            write_imports(source_file, destination_file)
 
-cph_help  .close()
-cph_config.close()
-cph_main  .close()
-tinyweb   .close()
+        for source_file in source_files:
+            for line in source_file.readlines():
+                if not is_comment_or_whitespace(line):
+                    destination_file.write(line)
+
+finally:
+    for source_file in source_files:
+        if not source_file.closed:
+            source_file.close()
 
